dd433914c01d43d52497f9dd7081a3cec4568280
Radu-Nicolae <rnicolae04@gmail.com>
2022-08-14 00:35:38 +0300

    Counter (individual project)

--- counter/app.js
+++ counter/app.js
@@ -1,4 +1,42 @@
-const valueSpan = document.getElementById("value");
+const value = document.querySelector("#value");
 const decreaseBtn = document.getElementById("decrease");
 const resetBtn = document.getElementById("reset");
 const increaseBtn = document.getElementById("increase");
+
+let count = 0;
+
+decreaseBtn.addEventListener("click", function (){
+    count--;
+    value.textContent = count;
+    colorIt(count);
+});
+
+increaseBtn.addEventListener("click", function(){
+    count++;
+    value.textContent = count;
+    colorIt(count);
+
+})
+
+resetBtn.addEventListener("click", function (){
+    value.textContent = 0;
+    count = 0;
+    colorIt(count);
+})
+
+
+
+
+function clear(){
+    return 0;
+}
+
+function colorIt(count){
+    if (count === 0){
+        value.style.color = "black";
+    } else if (count < 0){
+        value.style.color = "red";
+    } else {
+        value.style.color = "green";
+    }
+}
