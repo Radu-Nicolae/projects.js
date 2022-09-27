b88ca060fd9780ca7514ec27100a5be1c674c9d0
Radu-Nicolae <rnicolae04@gmail.com>
2022-09-27 23:15:44 +0200

    proj 7 finished by myself

--- 7_questions/app.js
+++ 7_questions/app.js
@@ -1,19 +1,21 @@
 const questions = document.querySelectorAll(".question");
 
-questions.forEach(function(question){
-    const btn = question.querySelector(".question-btn");
 
-    btn.addEventListener("click", function(){
+
+questions.forEach(function(question){
+    const button = question.querySelector(".question-btn");
+
+    button.addEventListener("click", function(){
         questions.forEach(function(item){
             if (item !== question){
                 item.classList.remove("show-text");
             }
         })
-
-        question.classList.toggle("show-text");
-
     })
-})
+
+    question.classList.toggle("show-text");
+
+});
 
 //useful
 //.question; .quesiton-btn
